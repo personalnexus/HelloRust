1464d4d1dc9cff4686f9cb551d5ddc7944404958
StefanH <personalnexus@users.noreply.github.com>
2021-07-27 21:43:18 +0200

    added examples of enum, struct, impl

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,43 @@
-fn say_hello(name: &str)
+#![allow(non_snake_case)]
+
+enum Niceness
 {
-    println!("Hello, {}. Nice to meet you", name);
+    NotNice,
+    Nice(String) // detail how nice the person is
+}
+
+struct Person
+{
+    name: String,
+    niceness: Niceness
+}
+
+impl Person{
+    fn little(&self, niceness: Niceness) -> Person
+    {
+        Person{
+            name: format!("Little {}", self.name),
+            niceness: niceness
+        }
+    }
+}
+
+fn say_hello(p: &Person)
+{
+    let greeting = match &p.niceness {
+        Niceness::NotNice => String::from("not nice"),
+        Niceness::Nice(how) => format!("{} nice", how)
+    };
+    println!("Hello, {}, {} to meet you.", p.name, greeting);
 }
 
 fn main() {
-    say_hello("World")
+    let bob = Person{
+        name: String::from("Bob"),
+        niceness: Niceness::Nice(String::from("very")),
+    };
+    say_hello(&bob);
+    let people = [bob];
+    let bobby = people[0].little(Niceness::NotNice);
+    say_hello(&bobby);
 }
