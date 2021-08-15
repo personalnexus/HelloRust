614fba57a77bbf752c84bdfb84a1e50fe5d78318
StefanH <personalnexus@users.noreply.github.com>
2021-08-15 19:38:32 +0200

    shorter closure syntax

--- src/team_book.rs
+++ src/team_book.rs
@@ -82,7 +82,7 @@ impl Members {
     }
 
     pub fn update(&mut self, name: &str, message: &str)  -> &mut Self {
-        let member = self.members_by_name.entry(String::from(name)).or_insert_with(|| { Member::new(name) });
+        let member = self.members_by_name.entry(String::from(name)).or_insert_with(|| Member::new(name));
         member.update(message);
         self
     }
